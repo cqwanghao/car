b9062d9f4d584b3ed466566b24f1c282ab7b2183
dianbowanghao <qwe875707281@163.com>
2016-12-23 15:53:05 +0800

    add file

---
+++ js/answer.js
@@ -0,0 +1,302 @@
+var carLogo = {
+    "logo1": [{
+        "first": "images/car_in/car_in1.png",
+        "second": "images/car_out/car_out1.png",
+        "area1": "images/choice/choice1-1.png",
+        "area2": "images/choice/choice1-2.png",
+        "area3": "images/choice/choice1-3.png",
+        "area4": "images/choice/choice1-4.png",
+        "proper": "images/choice/choice1-5.png",
+        "result": "btn3"
+    }],
+    "logo2": [{
+        "first": "images/car_in/car_in2.png",
+        "second": "images/car_out/car_out2.png",
+        "area1": "images/choice/choice2-1.png",
+        "area2": "images/choice/choice2-2.png",
+        "area3": "images/choice/choice2-3.png",
+        "area4": "images/choice/choice2-4.png",
+        "proper": "images/choice/choice2-5.png",
+        "result": "btn1"
+    }],
+    "logo3": [{
+        "first": "images/car_in/car_in3.png",
+        "second": "images/car_out/car_out3.png",
+        "area1": "images/choice/choice3-1.png",
+        "area2": "images/choice/choice3-2.png",
+        "area3": "images/choice/choice3-3.png",
+        "area4": "images/choice/choice3-4.png",
+        "proper": "images/choice/choice3-5.png",
+        "result": "btn3"
+    }],
+    "logo4": [{
+        "first": "images/car_in/car_in4.png",
+        "second": "images/car_out/car_out4.png",
+        "area1": "images/choice/choice4-1.png",
+        "area2": "images/choice/choice4-2.png",
+        "area3": "images/choice/choice4-3.png",
+        "area4": "images/choice/choice4-4.png",
+        "proper": "images/choice/choice4-5.png",
+        "result": "btn1"
+    }],
+    "logo5": [{
+        "first": "images/car_in/car_in5.png",
+        "second": "images/car_out/car_out5.png",
+        "area1": "images/choice/choice5-1.png",
+        "area2": "images/choice/choice5-2.png",
+        "area3": "images/choice/choice5-3.png",
+        "area4": "images/choice/choice5-4.png",
+        "proper": "images/choice/choice5-5.png",
+        "result": "btn2"
+    }],
+    "logo6": [{
+        "first": "images/car_in/car_in6.png",
+        "second": "images/car_out/car_out6.png",
+        "area1": "images/choice/choice6-1.png",
+        "area2": "images/choice/choice6-2.png",
+        "area3": "images/choice/choice6-3.png",
+        "area4": "images/choice/choice6-4.png",
+        "proper": "images/choice/choice6-5.png",
+        "result": "btn2"
+    }],
+    "logo7": [{
+        "first": "images/car_in/car_in7.png",
+        "second": "images/car_out/car_out7.png",
+        "area1": "images/choice/choice7-1.png",
+        "area2": "images/choice/choice7-2.png",
+        "area3": "images/choice/choice7-3.png",
+        "area4": "images/choice/choice7-4.png",
+        "proper": "images/choice/choice7-5.png",
+        "result": "btn2"
+    }],
+    "logo8": [{
+        "first": "images/car_in/car_in8.png",
+        "second": "images/car_out/car_out8.png",
+        "area1": "images/choice/choice8-1.png",
+        "area2": "images/choice/choice8-2.png",
+        "area3": "images/choice/choice8-3.png",
+        "area4": "images/choice/choice8-4.png",
+        "proper": "images/choice/choice8-5.png",
+        "result": "btn4"
+    }],
+    "logo9": [{
+        "first": "images/car_in/car_in9.png",
+        "second": "images/car_out/car_out9.png",
+        "area1": "images/choice/choice9-1.png",
+        "area2": "images/choice/choice9-2.png",
+        "area3": "images/choice/choice9-3.png",
+        "area4": "images/choice/choice9-4.png",
+        "proper": "images/choice/choice9-5.png",
+        "result": "btn1"
+    }],
+    "logo10": [{
+        "first": "images/car_in/car_in10.png",
+        "second": "images/car_out/car_out10.png",
+        "area1": "images/choice/choice10-1.png",
+        "area2": "images/choice/choice10-2.png",
+        "area3": "images/choice/choice10-3.png",
+        "area4": "images/choice/choice10-4.png",
+        "proper": "images/choice/choice10-5.png",
+        "result": "btn2"
+    }],
+    "logo11": [{
+        "first": "images/car_in/car_in11.png",
+        "second": "images/car_out/car_out11.png",
+        "area1": "images/choice/choice11-1.png",
+        "area2": "images/choice/choice11-2.png",
+        "area3": "images/choice/choice11-3.png",
+        "area4": "images/choice/choice11-4.png",
+        "proper": "images/choice/choice11-5.png",
+        "result": "btn4"
+    }],
+    "logo12": [{
+        "first": "images/car_in/car_in12.png",
+        "second": "images/car_out/car_out12.png",
+        "area1": "images/choice/choice12-1.png",
+        "area2": "images/choice/choice12-2.png",
+        "area3": "images/choice/choice12-3.png",
+        "area4": "images/choice/choice12-4.png",
+        "proper": "images/choice/choice12-5.png",
+        "result": "btn1"
+    }],
+    "logo13": [{
+        "first": "images/car_in/car_in13.png",
+        "second": "images/car_out/car_out13.png",
+        "area1": "images/choice/choice13-1.png",
+        "area2": "images/choice/choice13-2.png",
+        "area3": "images/choice/choice13-3.png",
+        "area4": "images/choice/choice13-4.png",
+        "proper": "images/choice/choice13-5.png",
+        "result": "btn2"
+    }],
+    "logo14": [{
+        "first": "images/car_in/car_in14.png",
+        "second": "images/car_out/car_out14.png",
+        "area1": "images/choice/choice14-1.png",
+        "area2": "images/choice/choice14-2.png",
+        "area3": "images/choice/choice14-3.png",
+        "area4": "images/choice/choice14-4.png",
+        "proper": "images/choice/choice14-5.png",
+        "result": "btn2"
+    }],
+    "logo15": [{
+        "first": "images/car_in/car_in15.png",
+        "second": "images/car_out/car_out15.png",
+        "area1": "images/choice/choice15-1.png",
+        "area2": "images/choice/choice15-2.png",
+        "area3": "images/choice/choice15-3.png",
+        "area4": "images/choice/choice15-4.png",
+        "proper": "images/choice/choice15-5.png",
+        "result": "btn2"
+    }],
+    "logo16": [{
+        "first": "images/car_in/car_in16.png",
+        "second": "images/car_out/car_out16.png",
+        "area1": "images/choice/choice16-1.png",
+        "area2": "images/choice/choice16-2.png",
+        "area3": "images/choice/choice16-3.png",
+        "area4": "images/choice/choice16-4.png",
+        "proper": "images/choice/choice16-5.png",
+        "result": "btn1"
+    }],
+    "logo17": [{
+        "first": "images/car_in/car_in17.png",
+        "second": "images/car_out/car_out17.png",
+        "area1": "images/choice/choice17-1.png",
+        "area2": "images/choice/choice17-2.png",
+        "area3": "images/choice/choice17-3.png",
+        "area4": "images/choice/choice17-4.png",
+        "proper": "images/choice/choice17-5.png",
+        "result": "btn4"
+    }],
+    "logo18": [{
+        "first": "images/car_in/car_in18.png",
+        "second": "images/car_out/car_out18.png",
+        "area1": "images/choice/choice18-1.png",
+        "area2": "images/choice/choice18-2.png",
+        "area3": "images/choice/choice18-3.png",
+        "area4": "images/choice/choice18-4.png",
+        "proper": "images/choice/choice18-5.png",
+        "result": "btn3"
+    }],
+    "logo19": [{
+        "first": "images/car_in/car_in19.png",
+        "second": "images/car_out/car_out19.png",
+        "area1": "images/choice/choice19-1.png",
+        "area2": "images/choice/choice19-2.png",
+        "area3": "images/choice/choice19-3.png",
+        "area4": "images/choice/choice19-4.png",
+        "proper": "images/choice/choice19-5.png",
+        "result": "btn1"
+    }],
+    "logo20": [{
+        "first": "images/car_in/car_in20.png",
+        "second": "images/car_out/car_out20.png",
+        "area1": "images/choice/choice20-1.png",
+        "area2": "images/choice/choice20-2.png",
+        "area3": "images/choice/choice20-3.png",
+        "area4": "images/choice/choice20-4.png",
+        "proper": "images/choice/choice20-5.png",
+        "result": "btn2"
+    }],
+    "logo21": [{
+        "first": "images/car_in/car_in21.png",
+        "second": "images/car_out/car_out21.png",
+        "area1": "images/choice/choice21-1.png",
+        "area2": "images/choice/choice21-2.png",
+        "area3": "images/choice/choice21-3.png",
+        "area4": "images/choice/choice21-4.png",
+        "proper": "images/choice/choice21-5.png",
+        "result": "btn2"
+    }],
+    "logo22": [{
+        "first": "images/car_in/car_in22.png",
+        "second": "images/car_out/car_out22.png",
+        "area1": "images/choice/choice22-1.png",
+        "area2": "images/choice/choice22-2.png",
+        "area3": "images/choice/choice22-3.png",
+        "area4": "images/choice/choice22-4.png",
+        "proper": "images/choice/choice22-5.png",
+        "result": "btn2"
+    }],
+    "logo23": [{
+        "first": "images/car_in/car_in23.png",
+        "second": "images/car_out/car_out23.png",
+        "area1": "images/choice/choice23-1.png",
+        "area2": "images/choice/choice23-2.png",
+        "area3": "images/choice/choice23-3.png",
+        "area4": "images/choice/choice23-4.png",
+        "proper": "images/choice/choice23-5.png",
+        "result": "btn4"
+    }],
+    "logo24": [{
+        "first": "images/car_in/car_in24.png",
+        "second": "images/car_out/car_out24.png",
+        "area1": "images/choice/choice24-1.png",
+        "area2": "images/choice/choice24-2.png",
+        "area3": "images/choice/choice24-3.png",
+        "area4": "images/choice/choice24-4.png",
+        "proper": "images/choice/choice24-5.png",
+        "result": "btn2"
+    }],
+    "logo25": [{
+        "first": "images/car_in/car_in25.png",
+        "second": "images/car_out/car_out25.png",
+        "area1": "images/choice/choice25-1.png",
+        "area2": "images/choice/choice25-2.png",
+        "area3": "images/choice/choice25-3.png",
+        "area4": "images/choice/choice25-4.png",
+        "proper": "images/choice/choice25-5.png",
+        "result": "btn3"
+    }],
+    "logo26": [{
+        "first": "images/car_in/car_in26.png",
+        "second": "images/car_out/car_out26.png",
+        "area1": "images/choice/choice26-1.png",
+        "area2": "images/choice/choice26-2.png",
+        "area3": "images/choice/choice26-3.png",
+        "area4": "images/choice/choice26-4.png",
+        "proper": "images/choice/choice26-5.png",
+        "result": "btn1"
+    }],
+    "logo27": [{
+        "first": "images/car_in/car_in27.png",
+        "second": "images/car_out/car_out27.png",
+        "area1": "images/choice/choice27-1.png",
+        "area2": "images/choice/choice27-2.png",
+        "area3": "images/choice/choice27-3.png",
+        "area4": "images/choice/choice27-4.png",
+        "proper": "images/choice/choice27-5.png",
+        "result": "btn1"
+    }],
+    "logo28": [{
+        "first": "images/car_in/car_in28.png",
+        "second": "images/car_out/car_out28.png",
+        "area1": "images/choice/choice28-1.png",
+        "area2": "images/choice/choice28-2.png",
+        "area3": "images/choice/choice28-3.png",
+        "area4": "images/choice/choice28-4.png",
+        "proper": "images/choice/choice28-5.png",
+        "result": "btn1"
+    }],
+    "logo29": [{
+        "first": "images/car_in/car_in29.png",
+        "second": "images/car_out/car_out29.png",
+        "area1": "images/choice/choice29-1.png",
+        "area2": "images/choice/choice29-2.png",
+        "area3": "images/choice/choice29-3.png",
+        "area4": "images/choice/choice29-4.png",
+        "proper": "images/choice/choice29-5.png",
+        "result": "btn2"
+    }],
+    "logo30": [{
+        "first": "images/car_in/car_in30.png",
+        "second": "images/car_out/car_out30.png",
+        "area1": "images/choice/choice30-1.png",
+        "area2": "images/choice/choice30-2.png",
+        "area3": "images/choice/choice30-3.png",
+        "area4": "images/choice/choice30-4.png",
+        "proper": "images/choice/choice30-5.png",
+        "result": "btn4"
+    }]
+}
---
+++ js/subject.js
@@ -0,0 +1,242 @@
+var carLogo = {
+    "logo1": [{
+        "first": "images/car_in/car_in1.png",
+        "second": "images/car_out/car_out1.png",
+        "area1": "images/choice/choice1-1.png",
+        "area2": "images/choice/choice1-2.png",
+        "area3": "images/choice/choice1-3.png",
+        "area4": "images/choice/choice1-4.png"
+    }],
+    "logo2": [{
+        "first": "images/car_in/car_in2.png",
+        "second": "images/car_out/car_out2.png",
+        "area1": "images/choice/choice2-1.png",
+        "area2": "images/choice/choice2-2.png",
+        "area3": "images/choice/choice2-3.png",
+        "area4": "images/choice/choice2-4.png"
+    }],
+    "logo3": [{
+        "first": "images/car_in/car_in3.png",
+        "second": "images/car_out/car_out3.png",
+        "area1": "images/choice/choice3-1.png",
+        "area2": "images/choice/choice3-2.png",
+        "area3": "images/choice/choice3-3.png",
+        "area4": "images/choice/choice3-4.png"
+    }],
+    "logo4": [{
+        "first": "images/car_in/car_in4.png",
+        "second": "images/car_out/car_out4.png",
+        "area1": "images/choice/choice4-1.png",
+        "area2": "images/choice/choice4-2.png",
+        "area3": "images/choice/choice4-3.png",
+        "area4": "images/choice/choice4-4.png"
+    }],
+    "logo5": [{
+        "first": "images/car_in/car_in5.png",
+        "second": "images/car_out/car_out5.png",
+        "area1": "images/choice/choice5-1.png",
+        "area2": "images/choice/choice5-2.png",
+        "area3": "images/choice/choice5-3.png",
+        "area4": "images/choice/choice5-4.png"
+    }],
+    "logo6": [{
+        "first": "images/car_in/car_in6.png",
+        "second": "images/car_out/car_out6.png",
+        "area1": "images/choice/choice6-1.png",
+        "area2": "images/choice/choice6-2.png",
+        "area3": "images/choice/choice6-3.png",
+        "area4": "images/choice/choice6-4.png"
+    }],
+    "logo7": [{
+        "first": "images/car_in/car_in7.png",
+        "second": "images/car_out/car_out7.png",
+        "area1": "images/choice/choice7-1.png",
+        "area2": "images/choice/choice7-2.png",
+        "area3": "images/choice/choice7-3.png",
+        "area4": "images/choice/choice7-4.png"
+    }],
+    "logo8": [{
+        "first": "images/car_in/car_in8.png",
+        "second": "images/car_out/car_out8.png",
+        "area1": "images/choice/choice8-1.png",
+        "area2": "images/choice/choice8-2.png",
+        "area3": "images/choice/choice8-3.png",
+        "area4": "images/choice/choice8-4.png"
+    }],
+    "logo9": [{
+        "first": "images/car_in/car_in9.png",
+        "second": "images/car_out/car_out9.png",
+        "area1": "images/choice/choice9-1.png",
+        "area2": "images/choice/choice9-2.png",
+        "area3": "images/choice/choice9-3.png",
+        "area4": "images/choice/choice9-4.png"
+    }],
+    "logo10": [{
+        "first": "images/car_in/car_in10.png",
+        "second": "images/car_out/car_out10.png",
+        "area1": "images/choice/choice10-1.png",
+        "area2": "images/choice/choice10-2.png",
+        "area3": "images/choice/choice10-3.png",
+        "area4": "images/choice/choice10-4.png"
+    }],
+    "logo11": [{
+        "first": "images/car_in/car_in11.png",
+        "second": "images/car_out/car_out11.png",
+        "area1": "images/choice/choice11-1.png",
+        "area2": "images/choice/choice11-2.png",
+        "area3": "images/choice/choice11-3.png",
+        "area4": "images/choice/choice11-4.png"
+    }],
+    "logo12": [{
+        "first": "images/car_in/car_in12.png",
+        "second": "images/car_out/car_out12.png",
+        "area1": "images/choice/choice12-1.png",
+        "area2": "images/choice/choice12-2.png",
+        "area3": "images/choice/choice12-3.png",
+        "area4": "images/choice/choice12-4.png"
+    }],
+    "logo13": [{
+        "first": "images/car_in/car_in13.png",
+        "second": "images/car_out/car_out13.png",
+        "area1": "images/choice/choice13-1.png",
+        "area2": "images/choice/choice13-2.png",
+        "area3": "images/choice/choice13-3.png",
+        "area4": "images/choice/choice13-4.png"
+    }],
+    "logo14": [{
+        "first": "images/car_in/car_in14.png",
+        "second": "images/car_out/car_out14.png",
+        "area1": "images/choice/choice14-1.png",
+        "area2": "images/choice/choice14-2.png",
+        "area3": "images/choice/choice14-3.png",
+        "area4": "images/choice/choice14-4.png"
+    }],
+    "logo15": [{
+        "first": "images/car_in/car_in15.png",
+        "second": "images/car_out/car_out15.png",
+        "area1": "images/choice/choice15-1.png",
+        "area2": "images/choice/choice15-2.png",
+        "area3": "images/choice/choice15-3.png",
+        "area4": "images/choice/choice15-4.png"
+    }],
+    "logo16": [{
+        "first": "images/car_in/car_in16.png",
+        "second": "images/car_out/car_out16.png",
+        "area1": "images/choice/choice16-1.png",
+        "area2": "images/choice/choice16-2.png",
+        "area3": "images/choice/choice16-3.png",
+        "area4": "images/choice/choice16-4.png"
+    }],
+    "logo17": [{
+        "first": "images/car_in/car_in17.png",
+        "second": "images/car_out/car_out17.png",
+        "area1": "images/choice/choice17-1.png",
+        "area2": "images/choice/choice17-2.png",
+        "area3": "images/choice/choice17-3.png",
+        "area4": "images/choice/choice17-4.png"
+    }],
+    "logo18": [{
+        "first": "images/car_in/car_in18.png",
+        "second": "images/car_out/car_out18.png",
+        "area1": "images/choice/choice18-1.png",
+        "area2": "images/choice/choice18-2.png",
+        "area3": "images/choice/choice18-3.png",
+        "area4": "images/choice/choice18-4.png"
+    }],
+    "logo19": [{
+        "first": "images/car_in/car_in19.png",
+        "second": "images/car_out/car_out19.png",
+        "area1": "images/choice/choice19-1.png",
+        "area2": "images/choice/choice19-2.png",
+        "area3": "images/choice/choice19-3.png",
+        "area4": "images/choice/choice19-4.png"
+    }],
+    "logo20": [{
+        "first": "images/car_in/car_in20.png",
+        "second": "images/car_out/car_out20.png",
+        "area1": "images/choice/choice20-1.png",
+        "area2": "images/choice/choice20-2.png",
+        "area3": "images/choice/choice20-3.png",
+        "area4": "images/choice/choice20-4.png"
+    }],
+    "logo21": [{
+        "first": "images/car_in/car_in21.png",
+        "second": "images/car_out/car_out21.png",
+        "area1": "images/choice/choice21-1.png",
+        "area2": "images/choice/choice21-2.png",
+        "area3": "images/choice/choice21-3.png",
+        "area4": "images/choice/choice21-4.png"
+    }],
+    "logo22": [{
+        "first": "images/car_in/car_in22.png",
+        "second": "images/car_out/car_out22.png",
+        "area1": "images/choice/choice22-1.png",
+        "area2": "images/choice/choice22-2.png",
+        "area3": "images/choice/choice22-3.png",
+        "area4": "images/choice/choice22-4.png"
+    }],
+    "logo23": [{
+        "first": "images/car_in/car_in23.png",
+        "second": "images/car_out/car_out23.png",
+        "area1": "images/choice/choice23-1.png",
+        "area2": "images/choice/choice23-2.png",
+        "area3": "images/choice/choice23-3.png",
+        "area4": "images/choice/choice23-4.png"
+    }],
+    "logo24": [{
+        "first": "images/car_in/car_in24.png",
+        "second": "images/car_out/car_out24.png",
+        "area1": "images/choice/choice24-1.png",
+        "area2": "images/choice/choice24-2.png",
+        "area3": "images/choice/choice24-3.png",
+        "area4": "images/choice/choice24-4.png"
+    }],
+    "logo25": [{
+        "first": "images/car_in/car_in25.png",
+        "second": "images/car_out/car_out25.png",
+        "area1": "images/choice/choice25-1.png",
+        "area2": "images/choice/choice25-2.png",
+        "area3": "images/choice/choice25-3.png",
+        "area4": "images/choice/choice25-4.png"
+    }],
+    "logo26": [{
+        "first": "images/car_in/car_in26.png",
+        "second": "images/car_out/car_out26.png",
+        "area1": "images/choice/choice26-1.png",
+        "area2": "images/choice/choice26-2.png",
+        "area3": "images/choice/choice26-3.png",
+        "area4": "images/choice/choice26-4.png"
+    }],
+    "logo27": [{
+        "first": "images/car_in/car_in27.png",
+        "second": "images/car_out/car_out27.png",
+        "area1": "images/choice/choice27-1.png",
+        "area2": "images/choice/choice27-2.png",
+        "area3": "images/choice/choice27-3.png",
+        "area4": "images/choice/choice27-4.png"
+    }],
+    "logo28": [{
+        "first": "images/car_in/car_in28.png",
+        "second": "images/car_out/car_out28.png",
+        "area1": "images/choice/choice28-1.png",
+        "area2": "images/choice/choice28-2.png",
+        "area3": "images/choice/choice28-3.png",
+        "area4": "images/choice/choice28-4.png"
+    }],
+    "logo29": [{
+        "first": "images/car_in/car_in29.png",
+        "second": "images/car_out/car_out29.png",
+        "area1": "images/choice/choice29-1.png",
+        "area2": "images/choice/choice29-2.png",
+        "area3": "images/choice/choice29-3.png",
+        "area4": "images/choice/choice29-4.png"
+    }],
+    "logo30": [{
+        "first": "images/car_in/car_in30.png",
+        "second": "images/car_out/car_out30.png",
+        "area1": "images/choice/choice30-1.png",
+        "area2": "images/choice/choice30-2.png",
+        "area3": "images/choice/choice30-3.png",
+        "area4": "images/choice/choice30-4.png"
+    }]
+}
